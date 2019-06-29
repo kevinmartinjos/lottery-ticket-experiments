57bc42dbfa0a881756202507284f4dd60dd9f61b
Kevin Martin Jose <youcancallmekevin@gmail.com>
2019-06-29 11:59:53 +0200

    Using SGD instead of Adam

--- experiment_base.py
+++ experiment_base.py
@@ -255,11 +255,16 @@ class ShuffleNetExperimentRunner(ExperimentRunner):
 
     def train(self, input_size, train_dataloader, validation_dataloader):
         criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate, weight_decay=self.reg)
-
+        # optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate, weight_decay=self.reg)
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-1, weight_decay=4e-5, momentum=0.9)
         training_start_time = time.time()
         best_validation_accuracy_so_far = 0
         for epoch in tqdm(range(self.num_epochs)):
+            if epoch == 150:
+                optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-2, weight_decay=4e-5, momentum=0.9)
+            elif epoch == 225:
+                optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-3, weight_decay=4e-5, momentum=0.9)
+
             for i, (images, labels) in enumerate(train_dataloader):
                 # Move tensors to the configured device
                 images = images.to(self.device)
