4497da59876c155759f578029284056b3d6e7a6e
Kevin Martin Jose <youcancallmekevin@gmail.com>
2019-07-21 10:31:19 +0200

    Changing the initialization weights for shufflenet

--- networks.py
+++ networks.py
@@ -246,8 +246,7 @@ class ShuffleNet(LotteryExperimentNetwork):
             m.weight.data.normal_(0.0, 1e-3)
             m.bias.data.fill_(0.)
         if type(m) == nn.Conv2d:
-            n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-            m.weight.data.normal_(0, math.sqrt(2. / n))
+            torch.nn.init.xavier_uniform(m.weight)
         elif type(m) == nn.BatchNorm2d:
             m.weight.data.fill_(1)
             m.bias.data.zero_()
